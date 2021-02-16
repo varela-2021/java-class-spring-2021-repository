877f0b3fded88531e32f603785fe1ebb353ae69c
Cvarela1993 <79024765+Cvarela1993@users.noreply.github.com>
2021-02-16 13:23:54 -0800

    Update A4_3.java

--- A4_3.java
+++ A4_3.java
@@ -1,34 +1,49 @@
+/*
+ Christian Varela
+ CS 111B
+ 2/16/21
+ Dave Harden
+ File name: A4_3.Java
+ 
+ Description of program: 
+
+
+
+*/
+
 import java.util.Scanner;
 
-int+push;
-
 public class A4_3 {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter a number greater than 0 (less than 1 to quit): ");
-		
+
 		int num = scan.nextInt();
 		int sum = 0;
 		int square;
-		
-		if(num > 1 || num == 1) {
-			for (int n =1 ; n < num+1 ; n++ ) {
+
+		while (num >= 1) {
+
+			for (int n = 1; n < num + 1; n++) {
 				square = n * n;
-				sum += square;
+				sum = square;
 			}
-			System.out.printf("The sum of the squares from 1 to %d",num);
-			System.out.printf(" is %d",sum);
-		
+			System.out.printf("The sum of the squares from 1 to %d", num);
+
+			System.out.printf(" is %d", sum);
+
+			num = scan.nextInt();
 		}
-		
-		
-			
+
 	}
 
 }
 
-
-/* Enter a number greater than 0 (less than 1 to quit): 4
-The sum of the squares from 1 to 4 is 30 */
+/*
+ * while(num>=1) {
+ * System.out.print("Enter a number greater than 0 (less than 1 to quit): ");
+ * num = scan.nextInt();
+ */
